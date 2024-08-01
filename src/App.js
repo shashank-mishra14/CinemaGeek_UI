e6f4cbb26ed19dc0d22b223876c43b05f7050c36
shashank01sj <shashank@sportjacks.com>
2024-08-01 13:14:32 +0530

    adding the hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-// import './App.css';
+import './App.css';
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
+import { Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Header/>
-     
+   <Hero/>
     </div>
   );
 }
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Hero.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import Carousel from 'react-material-ui-carousel'
+import {Paper} from '@mui/material'
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons"
+import {Button} from 'react-bootstrap'
+import {Link, useNavigate} from "react-router-dom"
+const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
+    function reviews(movieId) {
+        navigate(`/Reviews/${movieId}`)
+    }
+
+  return (
+    <div className='movie-carousel-container'>
+        <Carousel>
+            {
+                movies?.map((movie)=>{
+                    return (
+                        <Paper key={movie.imdbId}>
+                            <div className='movie-card-container'>
+                                <div className='movie-card' style={{"--img":'url(${movie.backdrops[0]})'}}>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt={movie.title}/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title} </h4>
+                                        <div className='movie-button-container'>
+                                            <Link>
+                                            <div className='play-button-icon-container'>
+                                                <FontAwesomeIcon className='play-button-icon' icon={faPlayCircle}/>
+
+                                            </div>
+                                            </Link>
+                                            <div className='movie-review-button-container'>
+                                            <Button variant='info' onClick={()=>reviews(movie.imdbId)}>
+                                                    Reviews
+                                            </Button>
+
+                                            </div>
+                                        </div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </Paper>
+                    )
+                })
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Hero
